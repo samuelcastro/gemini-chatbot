ce406982533a8e464f2690c665ae551b59bca4b9
Samuel Silva <samuelcastrosilva@gmail.com>
2025-05-13 07:34:02 -0400

    Added image

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = async () => {
       <div className="bg-background absolute top-0 left-0 w-dvw py-2 px-3 justify-between flex flex-row items-center z-30">
         <div className="flex flex-row gap-3 items-center">
           <div className="flex flex-row gap-2 items-center">
-            <Image
+            <img
               src="https://gemini-chatbot-linkeen.vercel.app/images/gemini-logo.png"
               height={20}
               width={20}
